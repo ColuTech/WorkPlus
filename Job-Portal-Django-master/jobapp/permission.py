531dbe41c27435cc184a9285259baa56f639bff0
Aaron Manalili <manalilia2@montclair.edu>
2023-04-28 19:11:39 -0400

    Update permission.py

--- Job-Portal-Django-master/jobapp/permission.py
+++ Job-Portal-Django-master/jobapp/permission.py
@@ -4,7 +4,7 @@ def user_is_employer(function):
 
     def wrap(request, *args, **kwargs):   
 
-        if request.user.role == 'business':
+        if request.user.role == 'employer':
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -17,7 +17,7 @@ def user_is_employee(function):
 
     def wrap(request, *args, **kwargs):    
 
-        if request.user.role == 'student':
+        if request.user.role == 'employee':
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied

24509c59c799e4b6b5b99790017ce6e5bc326236
Alan Montero <61355940+AlanMC937@users.noreply.github.com>
2023-05-01 15:41:11 -0400

    Updated settings for database

--- Job-Portal-Django-master/job/settings.py
+++ Job-Portal-Django-master/job/settings.py
@@ -86,21 +86,14 @@ WSGI_APPLICATION = 'job.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jobportal',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'workplus',
+        'USER': 'WorkplusAdmin',
+        'PASSWORD': 'workmas!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 #for debug toolbar
